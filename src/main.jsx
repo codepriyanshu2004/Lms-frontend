2419a080fceeb980ab7e4fd28c8084cdecc3211a
codepriyanshu2004 <priyanshubitturajs@gmail.com>
2024-11-26 18:28:38 +0530

    Added Sign in functionality

--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,13 @@ import store from './Redux/store.js'
 createRoot(document.getElementById('root')).render(
 
     <Provider store={store}>
+
  <BrowserRouter>
 
     <App />
+
     <Toaster/>
+    
     </BrowserRouter>
 
     </Provider>
