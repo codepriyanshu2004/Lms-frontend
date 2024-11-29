56722118cf4d84c1a896ccc875da4e8ae80a7436
codepriyanshu2004 <priyanshubitturajs@gmail.com>
2024-11-29 21:29:59 +0530

    Added course slice to redux

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './Slices/AuthSlice.js';
+import courseSliceReducer from "./Slices/CourseSlice.js" 
 
 const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        course:courseSliceReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
     devTools: true
