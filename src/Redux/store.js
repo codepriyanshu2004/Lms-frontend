22cdf2ebeb4c27b1790f6512e99ad52a8d47217b
codepriyanshu2004 <priyanshubitturajs@gmail.com>
2024-12-25 14:50:47 +0530

    Added lecture slice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,12 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './Slices/AuthSlice.js';
 import courseSliceReducer from "./Slices/CourseSlice.js" 
 import razorpayReducer from "./Slices/RezorpaySlice.js"
+import lectureSliceReducer from './Slices/LectureSlice';
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         course:courseSliceReducer,
-        razorpay:razorpayReducer
+        razorpay:razorpayReducer,
+        lecture: lectureSliceReducer,
+
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
     devTools: true
